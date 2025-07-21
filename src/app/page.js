bb9888375f0d4973b90aeba6e44405e271c3fb75
Victor Torres <clerossephirot@gmail.com>
2025-07-21 19:41:37 -0300

    chore: criada hierarquia de paginas e componentes

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,16 @@
+import Main from "./pages/main/Main";
+import Articles from "./pages/articles/Articles";
+import Experience from "./pages/experience/Experience";
+import Projects from "./pages/projects/Projects";
 
 
 export default function Home() {
   return (
     <>
+    <Main />
+    <Articles />
+    <Experience />
+    <Projects />
     </>
   );
 }
