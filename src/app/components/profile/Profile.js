3e589b0310f1311dccd3620622d2612ad22fa327
Victor Torres <clerossephirot@gmail.com>
2025-07-22 07:49:40 -0300

    chore: componente profile criado

---
+++ src/app/components/profile/Profile.js
@@ -0,0 +1,49 @@
+import React from 'react'
+import Image from 'next/image'
+import ProfileImage from '../../../../public/assets/profile-image.jpg'
+import GitHibIcon from '../../../../public/assets/gitHubRed.png'
+import LinkedInIcon from '../../../../public/assets/linkedinIcon.png'
+
+const Profile = () => {
+  return (
+    <div className="w-full">
+        <div className="w-70 mt-35 ml-40 bg-stone-500 rounded-2xl text-center p-5 flex flex-col items-center gap-4">
+            <div className="relative">
+                <Image
+                  src={ProfileImage}
+                  alt="Foto de Perfil de Victor M. Torres"
+                  width={150}
+                  height={150}
+                  className="rounded-3xl object-cover"
+                />
+            </div>
+            <h1 className="text-2xl font-bold text-white mt-4">Victor M. Torres</h1>
+            <p className="text-white">
+              Analista dedicado ao constante autodesenvolvimento, buscando
+              aprimorar habilidades técnicas e interpessoais. Movido pelo
+              propósito de impactar positivamente a sociedade.
+            </p>
+            <div className="flex items-center gap-6 mt-4">
+                <a href="https://github.com/vladsm007" target="_blank" rel="noopener noreferrer" aria-label="Perfil do GitHub de Victor M. Torres">
+                    <Image
+                      src={GitHibIcon}
+                      alt="Ícone do GitHub"
+                      width={30}
+                      height={30}
+                    />
+                </a>
+                <a href="https://linkedin.com/in/seu-usuario-aqui" target="_blank" rel="noopener noreferrer" aria-label="Perfil do LinkedIn de Victor M. Torres">
+                    <Image
+                      src={LinkedInIcon}
+                      alt="Ícone do LinkedIn"
+                      width={30}
+                      height={30}
+                    />
+                </a>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Profile
--- src/app/page.js
+++ src/app/page.js
@@ -3,12 +3,14 @@ import Articles from "./pages/articles/Articles";
 import Experience from "./pages/experience/Experience";
 import Projects from "./pages/projects/Projects";
 import Navbar from "./components/navbar/Navbar";
+import Profile from "./components/profile/Profile";
 
 
 export default function Home() {
   return (
     <>
     <Navbar />
+    <Profile />
     <Main />
     <Articles />
     <Experience />
