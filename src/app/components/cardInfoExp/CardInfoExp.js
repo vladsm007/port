c6ed1c69b94615c807690b88ebfdd7a609711719
Victor Torres <clerossephirot@gmail.com>
2025-07-22 21:06:41 -0300

    chore:pagina de experiencia e cards de experiencia criados

---
+++ src/app/components/cardInfoExp/CardInfoExp.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import Image from 'next/image'
+import Link from 'next/link'
+import OpenIcon from '../../../../public/assets/open-24.png'
+
+const CardInfoExp = ({
+    href = "#",
+    title,
+    text,
+    date,
+}) => {
+  return (
+    <Link href={href} passHref>
+        <div className=" hover:bg-gradient-to-r hover:bg-red-600 rounded-lg p-4 relative h-35 transition duration-200 ease-in-out">
+            <div className="flex flex-col">
+              <h6 className="text-2xl font-bold text-white mb-2">
+                {title}
+              </h6>
+              <p className="text-sm text-neutral-300">
+                {text}
+              </p>
+              <p className="text-sm pt-2 text-neutral-300">
+                {date}
+              </p>
+           </div>
+            <Image 
+            src={OpenIcon}
+            alt="Ir para experiência"
+            width={20}
+            height={20}
+            className="absolute top-2 right-2"
+            />
+        </div>
+     </Link>
+  )
+}
+
+export default CardInfoExp
--- src/app/experience/page.js
+++ src/app/experience/page.js
@@ -1,8 +1,38 @@
 import React from 'react'
+import CardInfoExp from '../components/cardInfoExp/CardInfoExp'
 
 const Experience = () => {
   return (
-    <div>Experience</div>
+  <div className="m-20 rounded-xl p-8 text-left shadow-md">
+      <h2 className="text-7xl font-bold text-white mb-6">
+        13 ANOS DE
+         <br></br>
+         <span className=" text-neutral-800">
+          EXPERIÊNCIA
+          </span>
+      </h2>
+      <section className="grid grid-cols-1 gap-6">
+        <CardInfoExp 
+        href='https://github.com/vladsm007'
+        title="Titulo da Experiência"
+        text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. In congue porta diam, vel lobortis tortor. Nulla id ornare arcu. Integer pharetra erat augue. Nullam hendrerit ornare velit nec interdum."
+        date="1987 to present"
+        />
+        <CardInfoExp 
+        href='https://github.com/vladsm007'
+        title="Titulo da Experiência"
+        text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. In congue porta diam, vel lobortis tortor. Nulla id ornare arcu. Integer pharetra erat augue. Nullam hendrerit ornare velit nec interdum."
+        date="1987 to present"
+        />
+        <CardInfoExp 
+        href='https://github.com/vladsm007'
+        title="Titulo da Experiência"
+        text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. In congue porta diam, vel lobortis tortor. Nulla id ornare arcu. Integer pharetra erat augue. Nullam hendrerit ornare velit nec interdum."
+        date="1987 to present"
+        />
+      </section>
+      
+    </div>
   )
 }
 
